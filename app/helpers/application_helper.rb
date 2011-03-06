e9f9f4e2f7eb8b8f86e8f411d7a2aa4f36aced56
Peter <pdtbell@gmail.com>
2011-03-06 14:38:41 +1100

    finished with users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  # Return a title on a per-page basis.
+  def title
+    base_title = "PlacementPlus"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def logo
+      image_tag("placement_plus_logo.png", :alt => "PlacementPlus")
+  end
+
 end
